dff0c6174d7946d024f3d3d3ad6b11dea6bff869
ivanovip1934 <ivanovip1934@outlook.com>
2019-06-17 20:27:28 +0300

    Написал программу из кники

---
+++ LeanReflection/MyClass.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeanReflection
+{
+    class MyClass
+    {
+        int x;
+        int y;
+
+        public MyClass(int i, int j) {
+            x = i;
+            y = j;
+        }
+
+        public int Sum() {
+            return x + y;
+        }
+
+        public bool IsBetween(int i) {
+            if (x < i && i < y) return true;
+            else return false;
+        }
+
+        public void Set(int a, int b) {
+            x = a;
+            y = b;
+        }
+
+        public void Set(double a, double b) {
+            x = (int)a;
+            y = (int)b;
+        }
+
+        public void Show() {
+            Console.WriteLine($"x: {x}, y: {y}");
+        }
+
+
+    }
+
+    
+}
--- LeanReflection/Program.cs
+++ LeanReflection/Program.cs
@@ -1,15 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 
 namespace LeanReflection
 {
+   
     class Program
     {
+
         static void Main(string[] args)
         {
+            Type t = typeof(MyClass);
+
+            Console.WriteLine("Анализ методов, определенных " +
+                "в классе " + t.Name);
+            Console.WriteLine();
+            Console.WriteLine("Поддерживаемые методы: ");
+            MethodInfo[] mi = t.GetMethods();
+
+            // Вывести методы, поддерживаемые в классе MyClass.
+            foreach (MethodInfo m in mi) {
+                // Вывести возвращаемый тип и имя каждого метода.
+                Console.Write(" " + m.ReturnType.Name + " " + m.Name + "(");
+
+                // Вывести параметры.
+                ParameterInfo[] pi = m.GetParameters();
+                for (int i = 0; i < pi.Length; i++) {
+                    Console.Write(pi[i].ParameterType.Name + " " + pi[i].Name);
+                    if (i + 1 < pi.Length)
+                        Console.Write(", ");
+                }
+                Console.WriteLine(")");
+
+                Console.WriteLine();                
+
+            }
+
+            Console.ReadKey();
+            
         }
     }
 }
