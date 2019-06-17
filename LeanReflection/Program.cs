ef863398ba4f69987dad32153fafdf19546d9c19
ivanovip1934 <ivanovip1934@outlook.com>
2019-06-17 20:46:09 +0300

    Изменил кодировку вовода на UTF8

--- LeanReflection/Program.cs
+++ LeanReflection/Program.cs
@@ -9,6 +9,7 @@ namespace LeanReflection
 
         static void Main(string[] args)
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
             Type t = typeof(MyClass);
 
             Console.WriteLine("Анализ методов, определенных " +
